b28f279e28f608f83e0869caf1e73e12eb4f19ef
PinballGraham <N/A>
2014-03-14 00:48:53 -0700

    Skeleton class for data writer
    Set up a proper data writer class declaration with a skeleton
    implementation.

--- ApplyJournal/DataWriter.cpp
+++ ApplyJournal/DataWriter.cpp
@@ -4,3 +4,21 @@
 // Output a data hierarchy of attribute/value pairs as formatted text.
 //
 // (c) 2014 Graham West
+
+// Class header, always comes first.
+#include "DataWriter.h"
+
+DataWriter::DataWriter()
+{
+}
+
+DataWriter::~DataWriter()
+{
+}
+
+bool DataWriter::Write(const DataHierarchy* const hierarchy, const QString& fileName)
+{
+	bool retval = false;
+
+	return retval;
+}
--- ApplyJournal/DataWriter.h
+++ ApplyJournal/DataWriter.h
@@ -4,3 +4,24 @@
 // Output a data hierarchy of attribute/value pairs as formatted text.
 //
 // (c) 2014 Graham West
+
+#if !defined(DATAWRITER_H)
+#define DATAWRITER_H
+
+// Application headers.
+#include "DataHierarchy.h"
+
+class DataWriter
+{
+public:
+	DataWriter();
+	~DataWriter();
+	
+	bool Write(const DataHierarchy* const hierarchy, const QString& fileName);
+	
+private:
+	DataWriter(const DataWriter& src);
+	DataWriter& operator=(const DataWriter& src);
+};
+
+#endif // DATAWRITER_H
